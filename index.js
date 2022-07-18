defa56c5fa55f4620870eff61d7adccecae491d1
Deden Rustiana <rustianadeden25@gmail.com>
2022-07-18 14:10:14 +0700

    Node Templating with PUG

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'pug');
+app.set('views','./views');
+
 app.get('/hello', function(req, res){
    res.send("Hello denrus!");
 });
 app.get('/denrus/:name', function(req, res){
    res.send("denrus!"+req.params.name);
 });
+
+app.get('/first_template', function(req, res){
+   res.render('first_view', {
+      name: "TutorialsPoint", 
+      url:"http://www.tutorialspoint.com"
+   });
+});
+
+app.get('/home', function(req, res){
+   res.render('home',{
+      user: {name: "Ayush", age: "20"}
+   });
+});
+
+app.get('/components', function(req, res){
+   res.render('content');
+});
+
+
 
 app.listen(3000);
