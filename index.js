a69e05211d524cf6b617b77cdc84685ddd4ac687
Deden Rustiana <rustianadeden25@gmail.com>
2022-07-19 04:58:36 +0700

    mongos

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ app.use(express.static('images'));
 app.set('view engine', 'pug');
 app.set('views','./views');
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/db_abc');
+
 app.get('/hello', function(req, res){
    res.send("Hello denrus!");
 });
@@ -36,7 +39,14 @@ app.get('/components', function(req, res){
 });
 
 app.get('/form', function(req, res){
-   res.render('form');
+   res.render('form', {
+      user : {
+         name: 'TJ',
+         email: 'tj@vision-media.ca',
+         city: 'Victoria',
+         province: 'BC',
+       }
+   });
 });
 
 // for parsing application/json
@@ -53,6 +63,41 @@ app.use(express.static('public'));
 app.post('/', function(req, res){
    console.log(req.body);
    res.send("recieved your request!");
+});
+
+var personSchema = mongoose.Schema({
+   name: String,
+   age: Number,
+   nationality: String
+});
+var Person = mongoose.model("Person", personSchema);
+
+
+app.get('/person', function(req, res){
+   res.render('person');
+});
+
+app.post('/person', function(req, res){
+   var personInfo = req.body; //Get the parsed information
+   
+   if(!personInfo.name || !personInfo.age || !personInfo.nationality){
+      res.render('show_message', {
+         message: "Sorry, you provided worng info", type: "error"});
+   } else {
+      var newPerson = new Person({
+         name: personInfo.name,
+         age: personInfo.age,
+         nationality: personInfo.nationality
+      });
+		
+      newPerson.save(function(err, Person){
+         if(err)
+            res.render('show_message', {message: "Database error", type: "error"});
+         else
+            res.render('show_message', {
+               message: "New person added", type: "success", person: personInfo});
+      });
+   }
 });
 
 app.listen(3000);
