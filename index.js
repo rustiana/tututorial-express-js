b09518e48f1a81925ba84e0f0d27d754f4fa7317
Deden Rustiana <rustianadeden25@gmail.com>
2022-07-18 09:32:10 +0700

    route

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res){
+app.get('/hello', function(req, res){
    res.send("Hello denrus!");
+});
+app.get('/denrus/:name', function(req, res){
+   res.send("denrus!"+req.params.name);
 });
 
 app.listen(3000);
